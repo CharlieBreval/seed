55387cff9993c9c36590ddaa9cc89707d305a698
CharlieBreval <cbreval@lafourchette.com>
2019-06-28 15:53:31 +0200

    Change home of prod

--- app/config/capistrano/stages/production.rb
+++ app/config/capistrano/stages/production.rb
@@ -1,12 +1,12 @@
 set :stage, :production
 
 set :ssh_user, 'charlieba'
-server 'ftp.cluster010.hosting.ovh.net', user: fetch(:ssh_user), roles: %w{web app db}
+server 'ssh.cluster010.hosting.ovh.net', user: fetch(:ssh_user), roles: %w{web app db}
 
 set :branch, 'master'
-set :deploy_to, '/homez.651/charlieba/charliebreval.com'
-set :tmp_dir, '/homez.651/charlieba/tmp'
-set :composer_path, '/homez.651/charlieba/composer.phar'
+set :deploy_to, '/homez.645/charlieba/charliebreval.com'
+set :tmp_dir, '/homez.645/charlieba/tmp'
+set :composer_path, '/homez.645/charlieba/composer.phar'
 
 SSHKit.config.command_map[:composer] = "/usr/local/php7.1/bin/php #{fetch(:composer_path)}"
 SSHKit.config.command_map[:php] = "/usr/local/php7.1/bin/php"
